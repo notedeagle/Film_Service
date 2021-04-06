4f1ff1d68ecbe9b4b3d3987ea9b3b72a0c0e246f
Dawid <notedeagle@gmail.com>
2021-04-06 16:42:45 +0200

    Initial commit

---
+++ src/main/java/com/kluczewski/filmservice/FilmserviceApplication.java
@@ -0,0 +1,13 @@
+package com.kluczewski.filmservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilmserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kluczewski/filmservice/FilmserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kluczewski.filmservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
